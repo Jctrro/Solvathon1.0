ab579ac2a2c87380d6fa55705944d2b63bd52e9c
Valen Mascarenhas <44286363+Jctrro@users.noreply.github.com>
2026-02-21 09:01:36 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -4,5 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def get_connection_repo():
+    return psycopg2.connect(os.getenv("REPO_DATABASE_URL"))
+
 def get_connection():
     return psycopg2.connect(os.getenv("DATABASE_URL"))
